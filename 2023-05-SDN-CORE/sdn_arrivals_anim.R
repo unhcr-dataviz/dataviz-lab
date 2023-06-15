8bddfd2a7fa2d8fa2fe3fa0b87961cef2176640e
Cedric Vidonne <cedric@vidonne.me>
2023-06-15 14:24:57 +0200

    add dynamic dates for SDN CORE and google sheet data

--- 2023-05-SDN-CORE/sdn_arrivals_anim.R
+++ 2023-05-SDN-CORE/sdn_arrivals_anim.R
@@ -2,10 +2,15 @@
 # Data source: ODP Sudan Situation
 
 # load packages -----------------------------------------------------------
-# pak::pkg_install("gganimate@1.0.7")
-# error with gganimate 1.0.8 so install older version
 library(tidyverse)
 library(scales)
+# error with gganimate 1.0.8 so install older version
+# if {pak} not installed first run:
+# install.packages('pak')
+# install gganimate 1.0.7:
+# pak::pkg_install("gganimate@1.0.7")
+# if error from pak try remotes package
+# remotes::install_version("gganimate", version = "1.0.7")
 library(gganimate)
 library(ragg)
 library(unhcrthemes)
@@ -16,12 +21,12 @@ library(unhcrthemes)
 
 df_raw <- read_csv("https://docs.google.com/spreadsheets/d/1dE3sADZOC5V_N5dnBDGjnafVEx8BvzXz3QAGfODwYfM/export?format=csv&id=1dE3sADZOC5V_N5dnBDGjnafVEx8BvzXz3QAGfODwYfM")
 
-df_raw
-
+# df_raw
 
 
 # wrangle data ------------------------------------------------------------
 
+# data for plot
 df_plot <-
   df_raw |>
   mutate(Date = paste0(Date, "-2023"),
@@ -32,11 +37,22 @@ df_plot <-
                names_to = "country",
                values_to = "pop_day")
 
+# last entry order by pop
 levels <- df_plot |>
   filter(date == max(date)) |>
   arrange(pop_day) |>
   pull(country)
 
+# last date label for file name
+last_date <-
+  df_plot |>
+  filter(date == max(date)) |>
+  distinct(date) |>
+  pull(date)
+
+# caption including last date
+caption <-
+  paste0("Source: UNHCR (", format(last_date,  '%d %B %Y'), ")")
 
 
 # plot unhcr --------------------------------------------------------------
@@ -50,13 +66,15 @@ theme_plot <- df_plot |>
   geom_line(linewidth = 0.75) +
   geom_point() +
   labs(title = "New arrivals from Sudan",
-       caption = "Source: UNHCR (as of 16 May 2023)") +
+       caption = caption) +
   unhcrthemes::scale_color_unhcr_d(nmax = 9, order = c(3, 1, 2, 5, 7, 9)) +
   scale_y_continuous(labels = scales::label_number(
     scale_cut = scales::cut_short_scale()),
     position = "right",
     expand = expansion(mult = c(0, 0.05)),
-    breaks = seq(0, 225e3, 25e3)
+    limits = c(NA, 500e3),
+    # adjust breaks if needed
+    # breaks = seq(0, 500e3, 100e3)
   ) +
   scale_x_date(date_breaks = "3 days", date_labels = "%d %b",
                expand = expansion(mult = 0.01)) +
@@ -74,7 +92,7 @@ anim <- animate(plot = theme_anim, duration = 10, fps = 30, end_pause = 90,
 # save --------------------------------------------------------------------
 
 save_animation(animation = anim,
-               file = "sdn_arrivals_2023-05-16.gif")
+               file = paste0("sdn_arrivals_", last_date, ".gif"))
 
 
 
